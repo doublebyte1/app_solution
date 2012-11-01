ccb381b068914a9abe43416776a0a182f7349653
Jo <doublebyte@gmail.com>
2012-11-01 12:16:19 +0100

    - Added folder with the physical files of the database

--- app_new/main.cpp
+++ app_new/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
     a.setApplicationName(QObject::tr("FAOFish CAS"));
     a.setOrganizationName(QObject::tr("FAO-FIRF"));
     a.setOrganizationDomain(QObject::tr("http://www.fao.org/"));
-    a.setApplicationVersion(QObject::tr("1.2"));
+    a.setApplicationVersion(QObject::tr("1.3"));
     //a.setApplicationVersion(QObject::tr("-DevBuild_") + QDateTime::currentDateTime().toString());
 
     Login l;
